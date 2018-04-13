729cecff89a23b043990301e40d9d4215e749891
Ung <908068685@qq.com>
2018-04-13 22:43:52 +0800

    Progress

---
+++ Source/androidbase/src/main/java/com/ung8023/androidbase/dialog/DialogActivity.java
@@ -0,0 +1,24 @@
+package com.ung8023.androidbase.dialog;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import cn.ung8023.androidstudy.activity.ActivityEntity;
+import cn.ung8023.androidstudy.activity.BaseMainActivity;
+
+/**
+ * @author feilang-liuyansong
+ * @date 2018/4/13 22:07
+ * @description
+ */
+
+public class DialogActivity extends BaseMainActivity {
+    @Override
+    protected List<ActivityEntity> getData() {
+        return new ArrayList<ActivityEntity>() {
+            {
+                add(new ActivityEntity("ProgressDialog", ProgressDialogActivity.class));
+            }
+        };
+    }
+}
